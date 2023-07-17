1a606e55495b1cd8920b95d20bd6c8d3c8486cbd
Noah3521 <noah3521@icloud.com>
2023-07-18 04:00:09 +0900

    34일차) sematic

---
+++ 3. 화면구현/day36/main.py
@@ -0,0 +1,12 @@
+
+        
+players = ["mumu", "soe", "poe", "kai", "mine"]
+callings = ["kai", "kai", "mine", "mine"]
+
+dict1 = {players[i] : i for i in range(len(players))}
+dict2 = {i : players[i]  for i in range(len(players))}
+print(dict1)
+
+for i in callings:
+
+print(dict)
